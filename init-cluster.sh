7dba7e1498856494ee08297cecee3038e19df9df
mohandab-dataeng <mohandab.dataeng@etik.com>
2026-07-08 21:25:31 +0200

    derniere modif

--- init-cluster.sh
+++ init-cluster.sh
@@ -6,7 +6,11 @@ done
 
 # Configuration des serveurs - metadonnées de la table de routage (commande mongosh)
 mongosh --host cfg_svr_1 --port 27018 --eval '
-rs.initiate({_id:"configRS",configsvr:true,members:[{_id:0,host:"cfg_svr_1:27018"},{_id:1,host:"cfg_svr_2:27019"},{_id:2,host:"cfg_svr_3:27020"}]})
+rs.initiate({_id:"configRS",configsvr:true,members:[
+{_id:0,host:"cfg_svr_1:27018"},
+{_id:1,host:"cfg_svr_2:27019"},
+{_id:2,host:"cfg_svr_3:27020"}
+]})
 '
 
 until mongosh --host shardParis_node1 --port 27031 --eval "db.runCommand('ping')" > /dev/null 2>&1; do
@@ -38,7 +42,7 @@ until mongosh --host mongos --port 27017 --eval "db.runCommand('ping')" > /dev/n
   sleep 2
 done
 
-# Connexion des shards (mongos) > hash
+# Connexion des shards (mongos) > hash sur city
 mongosh --host mongos --port 27017 --eval '
 sh.addShard("shardParisRS/shardParis_node1:27031,shardParis_node2:27032,shardParis_node3:27033")
 sh.addShard("shardLyonRS/shardLyon_node1:27041,shardLyon_node2:27042,shardLyon_node3:27043")
